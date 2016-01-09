bad9a3aa33467ba7b802227416cd2cdeb2160aa8
spring3th <dengchangqing@gmail.com>
2016-01-09 13:43:17 +0800

    add radio

--- LM/management/admin.py
+++ LM/management/admin.py
@@ -4,13 +4,17 @@ from management.models import *
 class MenberAdmin(admin.ModelAdmin):
     list_display =('menber_name','menber_address','menber_city','menber_store','menber_tel')
     search_fields = ('menber_name',)
-    def menber_store(self,obj):
-        return self.menber_store
+
 
 class CourseAdmin(admin.ModelAdmin):
     list_display =('course_name','course_subject','beizhu')
     search_fields =('course_name',)
 
+class SignAdmin(admin.ModelAdmin):
+    list_display = ('sign_mood','sign_date','course','menber')
+
 admin.site.register(Menbers,MenberAdmin)
 admin.site.register(Courses,CourseAdmin)
+admin.site.register(Sign,SignAdmin)
+
 
