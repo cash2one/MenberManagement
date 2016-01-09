c60049df40287ead29c067b606ce3ca35cf0cf36
spring3th <dengchangqing@gmail.com>
2016-01-09 11:03:33 +0800

    add radio

--- LM/management/admin.py
+++ LM/management/admin.py
@@ -11,17 +11,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_display =('course_name','course_subject','beizhu')
     search_fields =('course_name',)
 
-class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('teacher_name','teacher_organize','teacher_address','teacher_contact','teacher_tel','beizhu')
-    search_fields = ('teacher_name','teacher_organize')
-
-class StoreAdmin(admin.ModelAdmin):
-    list_display = ('store_name','store_brand','store_address','store_boss','store_tel')
-
-admin.site.register(Stores,StoreAdmin)
 admin.site.register(Menbers,MenberAdmin)
-admin.site.register(Teacher,TeacherAdmin)
 admin.site.register(Courses,CourseAdmin)
 admin.site.register(MyUser)
-admin.site.register(Book)
-admin.site.register(Img)
+
