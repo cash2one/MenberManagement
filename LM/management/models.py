b00e94fb91b3d820f795508f20a512209e62f528
spring3th <dengchangqing@gmail.com>
2016-01-10 22:44:35 +0800

    modify models

--- LM/management/models.py
+++ LM/management/models.py
@@ -2,6 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Courses(models.Model):
+    course_name = models.CharField(max_length=50,verbose_name=u"课程名")
+    course_subject = models.TextField(verbose_name=u"课程纲要")
+    teacher = models.TextField(verbose_name=u"老师")
+    organize = models.CharField(max_length=50,verbose_name=u"培训机构")
+    course_date = models.DateField(verbose_name=u"培训日期")
+    course_address = models.CharField(max_length=50,verbose_name=u"授课场地")
+    beizhu = models.TextField(verbose_name=u"备注")
+    
+    def __str__(self):
+        return self.course_name
+    class Meta:
+        verbose_name = "课程"
+        verbose_name_plural ="课程"
+
+class Sign(models.Model):
+    sign_mood = models.CharField(max_length=50,verbose_name=u"一句话")
+    sign_date = models.DateTimeField(auto_now_add=True,verbose_name=u"签到时间")
+   
+    def __str__(self):
+        return self.sign_mood
+    class Meta:
+        verbose_name = "签到"
+        verbose_name_plural ="签到"
+        ordering = ('sign_date',)
+
 class Menbers(models.Model):
     user = models.OneToOneField(User)
     permission = models.IntegerField()
@@ -12,6 +38,8 @@ class Menbers(models.Model):
     menber_store = models.CharField(max_length=50,verbose_name=u"店名")
     menber_brand = models.CharField(max_length=50,verbose_name=u"品牌")
     menber_typ = models.CharField(max_length = 60,verbose_name="级别")
+    courses = models.ManyToManyField(Courses)
+    sign = models.ForeignKey(Sign)
 
     def __str__(self):
         return u'%s %s' % (self.menber_name,self.menber_city)
@@ -19,32 +47,5 @@ class Menbers(models.Model):
         verbose_name = "学员"
         verbose_name_plural ="学员"
 
-class Sign(models.Model):
-    sign_mood = models.CharField(max_length=50,verbose_name=u"一句话")
-    sign_date = models.DateTimeField(auto_now_add=True,verbose_name=u"签到时间")
-    menber = models.ForeignKey(Menbers)
-    
-    def __str__(self):
-        return self.sign_mood
-    class Meta:
-        verbose_name = "签到"
-        verbose_name_plural ="签到"
-        ordering = ('sign_date',)
 
-
-class Courses(models.Model):
-    course_name = models.CharField(max_length=50,verbose_name=u"课程名")
-    course_subject = models.TextField(verbose_name=u"课程纲要")
-    teacher = models.TextField(verbose_name=u"老师")
-    organize = models.CharField(max_length=50,verbose_name=u"培训机构")
-    course_date = models.DateField(verbose_name=u"培训日期")
-    course_address = models.CharField(max_length=50,verbose_name=u"授课场地")
-    beizhu = models.TextField(verbose_name=u"备注")
-    sign = models.ManyToManyField(Sign)
-
-    def __str__(self):
-        return self.course_name
-    class Meta:
-        verbose_name = "课程"
-        verbose_name_plural ="课程"
         
