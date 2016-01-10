af5266d6b69922f4ecbed83f0ceba4d11183a238
spring3th <dengchangqing@gmail.com>
2016-01-10 10:08:12 +0800

    modify admin.py

--- LM/management/admin.py
+++ LM/management/admin.py
@@ -11,7 +11,7 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields =('course_name',)
 
 class SignAdmin(admin.ModelAdmin):
-    list_display = ('sign_mood','sign_date','course','menber')
+    list_display = ('sign_mood','sign_date')
 
 admin.site.register(Menbers,MenberAdmin)
 admin.site.register(Courses,CourseAdmin)
