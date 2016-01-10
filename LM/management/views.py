5b079d3e3524517063e0621c7d1f3b31c5c1058c
spring3th <dengchangqing@gmail.com>
2016-01-10 08:45:18 +0800

    add datetime

--- LM/management/views.py
+++ LM/management/views.py
@@ -168,7 +168,7 @@ def qiandao(req):
 		user = Menbers.objects.get(user__username=username)
 	else:
 		user=''
-	dt = datetime.now()
+	dt = datetime.datetime.now().strftime('%y-%m-%d %I:%M:%S %p') 
 
 	content = {'active_menu': 'qiandao', 'user': user,'datetime':dt}
 	return render_to_response('qiandao.html', content)
