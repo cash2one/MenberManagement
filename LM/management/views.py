e699c1d26af65b4802549d9679c0861c4dc55221
spring3th <dengchangqing@gmail.com>
2016-01-10 15:06:59 +0800

    add qiandao

--- LM/management/views.py
+++ LM/management/views.py
@@ -185,8 +185,8 @@ def qiandao(req):
 		post = req.POST
 		sign_mood = post.get('sign_mood','')
 		Id = post.get('course_id','')
-		cs = Courses.objects.get(pk=Id)
-		qiandao = Sign(menber=user,course=cs,sign_mood=sign_mood)
+		#cs = Courses.objects.get(pk=Id)
+		qiandao = Sign(menber=user,sign_mood=sign_mood)
 		qiandao.save()
 	content = {'active_menu': 'qiandao', 'user': user,'datetime':dt,'course':course_list,'status': status}
 	return render_to_response('qiandao.html', content, context_instance=RequestContext(req))
