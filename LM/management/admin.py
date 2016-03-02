ced6e18732125b9fca4b4a800f111333844cc04e
spring3th <dengchangqing@gmail.com>
2016-03-02 16:25:27 +0800

    modify menber model

--- LM/management/admin.py
+++ LM/management/admin.py
@@ -5,14 +5,14 @@ class MenberAdmin(admin.ModelAdmin):
     list_display =('menber_name','menber_address','menber_city','menber_store','menber_tel')
     search_fields = ('menber_name',)
 
-
+'''
 class CourseAdmin(admin.ModelAdmin):
     list_display =('course_name','course_subject','beizhu')
     search_fields =('course_name',)
 
 class SignAdmin(admin.ModelAdmin):
     list_display = ('sign_mood','sign_date')
-
+'''
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('tel','name','gift')
 
@@ -22,8 +22,9 @@ class FriendsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Menbers,MenberAdmin)
-admin.site.register(Courses,CourseAdmin)
-admin.site.register(Sign,SignAdmin)
+#admin.site.register(Courses,CourseAdmin)
+#admin.site.register(Sign,SignAdmin)
+admin.site.register(Personnel)
 admin.site.register(Person,PersonAdmin)
 admin.site.register(Friends,FriendsAdmin)
 
