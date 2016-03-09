eeaa9a3a519e3c03cd51ad56cde180893d71877c
spring3th <dengchangqing@gmail.com>
2016-03-09 14:54:11 +0800

    add  personnel html

--- LM/management/views.py
+++ LM/management/views.py
@@ -594,7 +594,7 @@ def modify_typ(req):
 		post = req.POST
 		pid = post.get('pid','')
 		personnel_type = post.get('personnel_typ','')
-		personnel_evaluate = post.get('evaluate','')
+		personnel_evaluate = post.get('evaluate','')+ timezone.localtime(timezone.now())
 		try:
 			Personnel.objects.filter(id=pid).update(personnel_typ=personnel_type,evaluate=personnel_evaluate)
 			status = "success"
