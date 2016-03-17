f4b105bdbf551d803a4d827b3e1995b133c1dfa6
spring3th <dengchangqing@gmail.com>
2016-03-17 22:02:35 +0800

    models add str function

--- LM/management/views.py
+++ LM/management/views.py
@@ -722,7 +722,7 @@ def employeemeeting(req):
 		return HttpResponseRedirect('/viewmeeting/')
 	try:
 		employee = Employee.objects.get(pk=Id)
-		weekmeeting = WeekMeeting.objects.filter(employee=employee)[0:2]
+		weekmeeting = WeekMeeting.objects.filter(employee=employee)[0:4]
 		pingyu = getSummary(employee)
 
 	except:
