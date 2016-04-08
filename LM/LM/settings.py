ea0e3f17a66b8a31bc8a0746bb97e964b4605ad1
spring3th <dengchangqing@gmail.com>
2016-04-08 23:29:22 +0800

    modify model admin

--- LM/LM/settings.py
+++ LM/LM/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'management',
-    'xadmin'
+    'management'
 )
 
 MIDDLEWARE_CLASSES = (
