f38bc912e691d59c7b190f067bea348e3a1a383c
spring3th <dengchangqing@gmail.com>
2016-04-16 10:21:07 +0800

    add  personnel html

--- LM/LM/settings.py
+++ LM/LM/settings.py
@@ -65,10 +65,10 @@ WSGI_APPLICATION = 'LM.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'management',
-        'USER':'spring',
-	'PASSWORD':'wf3531225',
-	'HOST':'64.207.188.243',
+        'NAME':'###',
+        'USER':'##',
+	'PASSWORD':'##',
+	'HOST':'###',
 	'PORT':'',
 	'OPTIONS':{'autocommit':True,}
     }
